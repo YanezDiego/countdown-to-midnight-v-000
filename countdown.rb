cb60bbef0c4c3d7ee3aacd167d407173fc88f422
Diego Yanez <diegosyanez92@gmail.com>
2018-03-27 16:23:52 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -4,7 +4,7 @@ def countdown (countdown_output)
   countdown_output = 10
     while n < 10
       puts "#{countdown_output} Seconds!"
-      n -= 1
+      countdown_output -= 1
     end
   return "HAPPY NEW YEAR!"
 end
